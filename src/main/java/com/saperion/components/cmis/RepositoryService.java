3b3e712d815175c5039bbce9f904b4b29bb3a0ba
Daniel Manzke <daniel.manzke@saperion.com>
2013-08-02 20:04:03 +0200

    added classic connector dependencies

--- src/main/java/com/saperion/components/cmis/RepositoryService.java
+++ src/main/java/com/saperion/components/cmis/RepositoryService.java
@@ -3,6 +3,7 @@ package com.saperion.components.cmis;
 import java.math.BigInteger;
 import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.chemistry.opencmis.commons.data.ExtensionsData;
 import org.apache.chemistry.opencmis.commons.data.ObjectData;
@@ -18,54 +19,95 @@ import org.apache.chemistry.opencmis.commons.enums.CapabilityJoin;
 import org.apache.chemistry.opencmis.commons.enums.CapabilityQuery;
 import org.apache.chemistry.opencmis.commons.enums.CapabilityRenditions;
 import org.apache.chemistry.opencmis.commons.enums.IncludeRelationships;
+import org.apache.chemistry.opencmis.commons.exceptions.CmisPermissionDeniedException;
 import org.apache.chemistry.opencmis.commons.impl.dataobjects.RepositoryCapabilitiesImpl;
 import org.apache.chemistry.opencmis.commons.impl.dataobjects.RepositoryInfoImpl;
 import org.apache.chemistry.opencmis.commons.impl.server.AbstractCmisService;
 import org.apache.chemistry.opencmis.commons.server.CallContext;
 
+import com.saperion.connector.authentication.Credentials;
+import com.saperion.connector.authentication.LicenseType;
+import com.saperion.connector.authentication.Session;
+import com.saperion.connector.authentication.keys.UsernamePasswordKey;
+import com.saperion.connector.pool.ConnectionPoolUtil;
+import com.saperion.connector.pool.PooledSession;
+
 /**
  * CMIS Service Implementation.
  */
 public class RepositoryService extends AbstractCmisService {
+	
+	public static final String ROOT_ID = "$$$ROOT$$$";
 
     private CallContext context;
-
-    // --- Call Context ---
-
-    /**
-     * Sets the call context.
-     * 
-     * This method should only be called by the service factory.
-     */
-    public void setCallContext(CallContext context) {
-        this.context = context;
-    }
+    
+    private PooledSession session;
+    
+    private Credentials credentials;
+    
+    private ConnectionPoolUtil pool;
+    
+    public RepositoryService(CallContext context, ConnectionPoolUtil pool) {
+		super();
+		this.context = context;
+		this.pool = pool;
+		this.credentials = new UsernamePasswordKey(context.getUsername(), context.getPassword(), LicenseType.INDEX, "");
+	}
 
     /**
      * Gets the call context.
      */
-    public CallContext getCallContext() {
+    public CallContext getContext() {
         return context;
+    }
+    
+    public PooledSession getSession() {
+		return session;
+	}
+    
+    public void connect() {
+        Session session = null;
+        try {
+			session = pool.borrowObject(credentials);
+			if(session == null) {
+				throw new CmisPermissionDeniedException("User couldn't be logged in.");
+			}
+		} catch (Exception e) {
+			throw new CmisPermissionDeniedException("User couldn't be logged in.");
+		}
+    }
+    
+    @Override
+    public void close() {
+    	if(session != null) {
+    		try {
+				pool.returnObject(credentials, session);
+			} catch (Exception e) {
+				// ignore?
+				e.printStackTrace();
+			}    		
+    	}
     }
 
     // --- CMIS Operations ---
 
     @Override
     public List<RepositoryInfo> getRepositoryInfos(ExtensionsData extension) {
+    	connect();
         // very basic repository info set up
         RepositoryInfoImpl repositoryInfo = new RepositoryInfoImpl();
 
-        repositoryInfo.setId("repository1");
-        repositoryInfo.setName("cmis");
-        repositoryInfo.setDescription("This is my first repository!");
+        repositoryInfo.setId("examplev7");
+        repositoryInfo.setName("examplev7");
+        repositoryInfo.setDescription("This is the repository representing the example v7");
 
-        repositoryInfo.setCmisVersionSupported("1.0");
+        repositoryInfo.setCmisVersionSupported("1.1");
 
-        repositoryInfo.setProductName("My Document Management System");
-        repositoryInfo.setProductVersion("0.1");
-        repositoryInfo.setVendorName("My Company");
+        repositoryInfo.setProductName("SAPERION");
+        repositoryInfo.setProductVersion("7.5");
+        repositoryInfo.setVendorName("SAPERION AG");
 
-        repositoryInfo.setRootFolder("1234567890");
+        repositoryInfo.setRootFolder(ROOT_ID);
 
         repositoryInfo.setThinClientUri("");
 
@@ -81,8 +123,8 @@ public class RepositoryService extends AbstractCmisService {
         capabilities.setCapabilityQuery(CapabilityQuery.NONE);
         capabilities.setCapabilityChanges(CapabilityChanges.NONE);
         capabilities.setCapabilityContentStreamUpdates(CapabilityContentStreamUpdates.ANYTIME);
-        capabilities.setSupportsGetDescendants(true);
-        capabilities.setSupportsGetFolderTree(true);
+        capabilities.setSupportsGetDescendants(false);
+        capabilities.setSupportsGetFolderTree(false);
         capabilities.setCapabilityRendition(CapabilityRenditions.NONE);
 
         return Collections.singletonList((RepositoryInfo) repositoryInfo);
@@ -91,12 +133,14 @@ public class RepositoryService extends AbstractCmisService {
     @Override
     public TypeDefinitionList getTypeChildren(String repositoryId, String typeId, Boolean includePropertyDefinitions,
             BigInteger maxItems, BigInteger skipCount, ExtensionsData extension) {
+    	connect();
         // TODO implement
         return null;
     }
 
     @Override
     public TypeDefinition getTypeDefinition(String repositoryId, String typeId, ExtensionsData extension) {
+    	connect();
         // TODO implement
         return null;
     }
@@ -105,6 +149,7 @@ public class RepositoryService extends AbstractCmisService {
     public ObjectInFolderList getChildren(String repositoryId, String folderId, String filter, String orderBy,
             Boolean includeAllowableActions, IncludeRelationships includeRelationships, String renditionFilter,
             Boolean includePathSegment, BigInteger maxItems, BigInteger skipCount, ExtensionsData extension) {
+    	connect();
         // TODO implement
         return null;
     }
@@ -113,6 +158,7 @@ public class RepositoryService extends AbstractCmisService {
     public List<ObjectParentData> getObjectParents(String repositoryId, String objectId, String filter,
             Boolean includeAllowableActions, IncludeRelationships includeRelationships, String renditionFilter,
             Boolean includeRelativePathSegment, ExtensionsData extension) {
+    	connect();
         // TODO implement
         return null;
     }
@@ -121,8 +167,8 @@ public class RepositoryService extends AbstractCmisService {
     public ObjectData getObject(String repositoryId, String objectId, String filter, Boolean includeAllowableActions,
             IncludeRelationships includeRelationships, String renditionFilter, Boolean includePolicyIds,
             Boolean includeAcl, ExtensionsData extension) {
+    	connect();
         // TODO implement
         return null;
     }
-
 }
--- src/main/java/com/saperion/components/cmis/RepositoryServiceFactory.java
+++ src/main/java/com/saperion/components/cmis/RepositoryServiceFactory.java
@@ -2,11 +2,20 @@ package com.saperion.components.cmis;
 
 import java.math.BigInteger;
 import java.util.Map;
+import java.util.Properties;
 
+import org.apache.chemistry.opencmis.commons.exceptions.CmisPermissionDeniedException;
 import org.apache.chemistry.opencmis.commons.impl.server.AbstractServiceFactory;
 import org.apache.chemistry.opencmis.commons.server.CallContext;
 import org.apache.chemistry.opencmis.commons.server.CmisService;
 import org.apache.chemistry.opencmis.server.support.CmisServiceWrapper;
+
+import com.saperion.connector.authentication.LicenseType;
+import com.saperion.connector.authentication.Session;
+import com.saperion.connector.authentication.keys.UsernamePasswordKey;
+import com.saperion.connector.pool.ConnectionPoolUtil;
+import com.saperion.connector.pool.PooledSession;
+import com.saperion.connector.pool.exceptions.FactoryException;
 
 /**
  * CMIS Service Factory.
@@ -27,38 +36,36 @@ public class RepositoryServiceFactory extends AbstractServiceFactory {
 
     /** Default depth value for getDescendants(). */
     private static final BigInteger DEFAULT_DEPTH_OBJECTS = BigInteger.valueOf(10);
+    
+    public static final String SESSION = "spn:session";
+    
+    private static ConnectionPoolUtil POOL = new ConnectionPoolUtil();
 
     @Override
     public void init(Map<String, String> parameters) {
+    	Properties configuration = new Properties();
+    	configuration.putAll(parameters);
+    	POOL.initialize(configuration);
     }
 
     @Override
     public void destroy() {
+    	try {
+			POOL.shutdown();
+		} catch (FactoryException e) {
+			// ignore
+			e.printStackTrace();
+		}
     }
 
     @Override
     public CmisService getService(CallContext context) {
-        // authentication can go here
-//        String user = context.getUsername();
-//        String password = context.getPassword();
+        RepositoryService service = new RepositoryService(context, POOL);
 
-        // if the authentication fails, throw a CmisPermissionDeniedException
-
-        // create a new service object
-        // (can also be pooled or stored in a ThreadLocal)
-        RepositoryService service = new RepositoryService();
-
-        // add the CMIS service wrapper
-        // (The wrapper catches invalid CMIS requests and sets default values
-        // for parameters that have not been provided by the client.)
         CmisServiceWrapper<RepositoryService> wrapperService = 
                 new CmisServiceWrapper<RepositoryService>(service,
                 DEFAULT_MAX_ITEMS_TYPES, DEFAULT_DEPTH_TYPES, DEFAULT_MAX_ITEMS_OBJECTS, DEFAULT_DEPTH_OBJECTS);
 
-        // hand over the call context to the service object
-        service.setCallContext(context);
-
         return wrapperService;
-    }
-
+    }    
 }
